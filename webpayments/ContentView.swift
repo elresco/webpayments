8d6b3e842d1f9d6e288b99ef2db09c0ddaf0c5f9
lresco <luis.rebla@gmail.com>
2025-05-09 18:39:40 +0200

    Initial Commit

---
+++ webpayments/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  webpayments
+//
+//  Created by lresco on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ webpayments/webpaymentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  webpaymentsApp.swift
+//  webpayments
+//
+//  Created by lresco on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct webpaymentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
